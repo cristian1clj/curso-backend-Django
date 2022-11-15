054db1022d03a3e95c893d0b1bfad6ad18e92376
cristian1clj <cristian1clj@gmail.com>
2022-11-15 13:08:10 -0500

    details, results, and vote views from polls was created

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Pagina principal de premios platzi app")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Pregunta #{question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Resultados de la pregunta #{question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Votando en la pregunta #{question_id}")
