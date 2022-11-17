630dc1c075b91cae43f6c5251024d78d976b8232
cristian1clj <cristian1clj@gmail.com>
2022-11-17 11:46:32 -0500

    polls index.html, views.index and detail.html, views.detail

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Pagina principal de premios platzi app")
+    latest_question_list = Question.objects.all()
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Pregunta #{question_id}")
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, id=question_id)
+    choices_list = question.choice_set.all()
+    
+    context = {
+        'question': question, 
+        'choices_list': choices_list
+    }
+    
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, question_id):
