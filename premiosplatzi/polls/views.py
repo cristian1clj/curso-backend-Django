a2c8c31234bff1dd884bc092f41f754827b45ee1
cristian1clj <cristian1clj@gmail.com>
2022-11-22 12:54:57 -0500

    polls views.vote, results.htmh, and view.results

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -14,7 +15,6 @@ def index(request):
 
 
 def detail(request, question_id):
-    # question = Question.objects.get(id=question_id)
     question = get_object_or_404(Question, id=question_id)
     choices_list = question.choice_set.all()
     
@@ -27,8 +27,32 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Resultados de la pregunta #{question_id}")
+    question = Question.objects.get(id=question_id)
+    choices_list = question.choice_set.all()
+    
+    context = {
+        'question': question, 
+        'choices_list': choices_list
+    }
+    
+    return render(request, "polls/results.html", context)
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Votando en la pregunta #{question_id}")
+    question = get_object_or_404(Question, id=question_id)
+    choices_list = question.choice_set.all()
+    
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question, 
+            'choices_list': choices_list, 
+            'error_message': "No elegiste una respuesta"
+        }
+        return render(request, "polls/detail.html", context)
+    else:
+        selected_choice.votes +=  1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
